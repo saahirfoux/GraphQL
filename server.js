2eec1d3ba25e508ca062cdf1520e90f4c18fa820
saahirfoux <saahirhfoux@gmail.com>
2020-08-13 05:10:59 -0400

    Creating callback page, and redirect

--- server.js
+++ server.js
@@ -1,32 +1,46 @@
 const express = require('express'),
-cors = require('cors'),
 resolvers = require('./resolvers'),
 typeDefs = require('./typeDefs'),
+axios = require('axios'),
 { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+app.use(express.static('public'))
 
 const PORT = 3001;
 
-const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers
-})
-
-apolloServer.applyMiddleware({app, path: '/challenge'});
 
 
-app.use('/', (req, res) => {
-    res.redirect('/challenge');
-});
+//apolloServer.applyMiddleware({app, path: '/challenge'});
 
+  app.get('/oauth/callback', (req, res) => {
+    const requestToken = req.query.code
+    const clientID = '872ca578affba5d8f86e';
+    const clientSecret = 'f9386d42c7ddb9c77a168c510ee2fd8654005571';
+    axios({
+      method: 'POST',
+      url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+      headers: {
+        accept: 'application/json'
+      }
+    }).then(({data}) => {
+      // extract the token from the response
+      const accessToken = data.access_token
+
+      // Do not start and apply the apollo server until after authentication
+      const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
+      });
+      apolloServer.applyMiddleware({app, path: '/challenge'});
+
+      res.redirect(`/callback.html?access_token=${accessToken}`)
+    })
+  })
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
-    console.log(`GraphQL endpoint is: ${apolloServer.graphqlPath}`);
 });
 
 
