93d472f3e86e8bae2378a8a5f1acf73b0becdca5
saahirfoux <saahirhfoux@gmail.com>
2020-08-13 02:04:33 -0400

    Update & Delete
    Adding update and delete handlers
    Added validators on update handler

--- server.js
+++ server.js
@@ -51,9 +51,35 @@ const typeDefs = gql`
         name: String
         email: String
         body: String
-    }                  
+    }
+    type Mutation {
+        updatePost(input: UpdatePostInput!): Post
+        deletePost(id: ID!): Post
+    }
+    input UpdatePostInput {
+        id: ID!
+        body: String
+        title: String
+    }
 `;
+const verifyInput = ({body, title}) => {
+    // if body or title exists, and it is an empty string, throw an error
+    let payload = {};
 
+    if (typeof body !== undefined && body.length === 0) {
+        throw new Error("Please enter desired text to update the contents of your post");
+    } else {
+        payload.body = body;
+    }
+    if (typeof title !== undefined && title.length === 0) {
+        throw new Error("Every post must have a title. Please enter it now");
+    } else if (typeof title !== undefined && title.length < 3) {
+        throw new Error("Minimum allowed length is 3 characters.");
+    } else {
+        payload.title = title;
+    }
+    return payload;
+  };
 const resolvers = {
     Query: {
         users: async () => {
@@ -103,6 +129,27 @@ const resolvers = {
             return await fetch('https://jsonplaceholder.typicode.com/posts?userId=' + id)
                 .then(response => response.json())
                 .then(posts => {return posts});
+        }
+    },
+    Mutation: {
+        updatePost: async (_, {input}) => {
+            const payload = verifyInput(input);
+
+            return await fetch('https://jsonplaceholder.typicode.com/posts/' + input.id,
+            {
+                method: 'PATCH',
+                body: JSON.stringify(payload),
+                headers: {
+                  "Content-type": "application/json; charset=UTF-8"
+                }
+            }).then(response => response.json())
+            .then(json => json);
+        },
+
+        deletePost: async (_, {id}) => {
+            return await fetch('https://jsonplaceholder.typicode.com/posts/' + id, {
+                method: 'DELETE'
+            });
         }
     }
 };
